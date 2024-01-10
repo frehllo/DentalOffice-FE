1d5c6ff7282867887a690f33d16fcc31a526307e
frehllo <narcisbuzatu2003@gmail.com>
2024-01-10 08:29:21 +0100

    to resolve color ecc..

--- src/app/core/components/standalones/home-section-card/home-section-card.component.ts
+++ src/app/core/components/standalones/home-section-card/home-section-card.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -5,7 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-home-section-card',
   standalone: true,
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, CommonModule],
   templateUrl: './home-section-card.component.html',
   styleUrl: './home-section-card.component.scss'
 })
