7f2895b2e49888c8830ae2831925186026c2c4de
frehllo <narcisbuzatu2003@gmail.com>
2024-06-08 12:37:50 +0200

    da capire come gestire electron, se gestirlo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+/* const { app, BrowserWindow } = require('electron')
 
 let win;
 
@@ -16,10 +16,44 @@ function CreateWindow() {
 
 app.whenReady().then(() => {
     CreateWindow()
+}) */
+
+const { app, BrowserWindow } = require('electron')
+const url = require("url");
+const path = require("path");
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/dental-office-fe/browser/index.html`),
+            protocol: "file:",
+            slashes: true
+        })
+    );
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
 })
 
-app.filter('to_trusted', ['$sce', function ($sce) {
-    return function (text) {
-        return $sce.trustAsHtml(text);
-    };
-}]);
+app.on('activate', function () {
+    if (mainWindow === null) createWindow()
+})
