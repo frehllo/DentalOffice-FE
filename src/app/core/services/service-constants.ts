30b1a6bf5ff0ded48e40d485baeec907d341be6b
narcisbuzatu <narcisbuzatu2003@gmail.com>
2026-01-02 04:06:17 +0100

    api url

--- src/app/core/services/service-constants.ts
+++ src/app/core/services/service-constants.ts
@@ -1,2 +1,3 @@
-// export const API_URL = "http://localhost:7020/api"
-export const API_URL = "http://localhost:5170/api"
+//export const API_URL = "http://localhost:7020/api"
+//export const API_URL = "http://localhost:5170/api"
+export const API_URL = "http://localhost:5000/api"
